7338b9c3661ce8b94363b02eba7262c3a83d79fd
Anh Le <haianhltr@yahoo.com.vn>
2022-08-03 18:21:52 +0700

    create stake account

--- create_stake_acount.js
+++ create_stake_acount.js
@@ -1,4 +1,4 @@
-const { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL } = require("@solana/web3.js")
+const { Connection, clusterApiUrl, Keypair, LAMPORTS_PER_SOL, StakeProgram, Authorized, Lockup, sendAndConfirmRawTransaction, sendAndConfirmTransaction } = require("@solana/web3.js")
 
 const main = async () => {
     const connection = new Connection(clusterApiUrl('devnet'),"processed")
@@ -8,9 +8,30 @@ const main = async () => {
     const balance = await connection.getBalance(wallet.publicKey);
     console.log(balance);
 
+    //create stake account
+    const stakeAccount = Keypair.generate();
+    const minimumRent = await connection.getMinimumBalanceForRentExemption(StakeProgram.space);
+    const amountUserWantsToStake = 0.5 * LAMPORTS_PER_SOL;
+    const amountToStake = minimumRent + amountUserWantsToStake;
+    const createStakeAccountTx = StakeProgram.createAccount(
+        {
+            authorized: new Authorized(wallet.publicKey, wallet.publicKey),
+            fromPubkey: wallet.publicKey,
+            lamports: amountToStake,
+            lockup: new Lockup(0,0,wallet.publicKey),
+            stakePubkey: stakeAccount.publicKey,
 
-    
-}
+        }
+    )
+    const createStakeAccountTxId = await sendAndConfirmTransaction(connection, createStakeAccountTx, [wallet,stakeAccount]);
+    console.log(`Stake Account created. Tx id: ${createStakeAccountTxId}`)
+    let stakeBalance = await connection.getBalance(stakeAccount.publicKey);
+    console.log(`Stake Account balance: ${stakeBalance/ LAMPORTS_PER_SOL} SOL`)
+
+    let stakeStatus = await connection.getStakeActivation(stakeAccount.publicKey);
+    console.log(`Stake Account status: ${stakeStatus.state}`);
+
+};
 
 
 const runMain = async () => {
